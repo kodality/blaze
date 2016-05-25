2507050518e66c3f58bac81eb26d62e3d0cc5682
mrcerf <mrcerf@gmail.com>
2016-05-25 11:24:55 +0300

    dockers

--- etc/docker/postgres/run-postgres.sh
+++ etc/docker/postgres/run-postgres.sh
@@ -7,4 +7,4 @@ DB_NAME="blazedb"
 docker run -e TZ=Europe/Tallinn -e DB_NAME=$DB_NAME -e POSTGRES_PASSWORD=mysecretpassword -p $LOCAL_PORT:5432 \
  -v `pwd`/`dirname $0`/docker-entrypoint-initdb.d:/docker-entrypoint-initdb.d \
  --restart=unless-stopped \
- --name $CONTAINER_NAME -d blaze/postgres
+ --name $CONTAINER_NAME -d postgres:9.5.2
