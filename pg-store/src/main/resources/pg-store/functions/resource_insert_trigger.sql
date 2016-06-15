0a63a4b01825ebbfa7de2ae8c485a319ec21d0df
mrcerf <mrcerf@gmail.com>
2016-06-15 13:00:36 +0300

    trigger fix

--- pg-store/src/main/resources/pg-store/functions/resource_insert_trigger.sql
+++ pg-store/src/main/resources/pg-store/functions/resource_insert_trigger.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION resource_insert_trigger() RETURNS TRIGGER AS 
 $$
 BEGIN
-  UPDATE resource SET sys_status = 'T' WHERE id = new.id AND type = new.type AND sys_status != 'T';
+  UPDATE resource SET sys_status = 'T' WHERE id = new.id AND type = new.type;
   EXECUTE 'INSERT INTO ' || lower(new.type) ||' VALUES (($1).*)' USING NEW ;
   return null;
 END;
