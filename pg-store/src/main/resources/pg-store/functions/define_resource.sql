c423d24e2cf87f3abdc02cf143115c679b518b72
Igor Bossenko <igor@Igors-MBP.lan>
2017-11-11 20:44:00 +0200

    upgrade to PG10

--- pg-store/src/main/resources/pg-store/functions/define_resource.sql
+++ pg-store/src/main/resources/pg-store/functions/define_resource.sql
@@ -8,12 +8,12 @@ BEGIN
     RETURN;
   END IF;
   
-  EXECUTE FORMAT('create table %s (like resource including all)', _tbl_name);
-  EXECUTE FORMAT('alter  table %s add constraint %s_type_check check(type=%L)', _tbl_name, _tbl_name, _type);
-  EXECUTE FORMAT('alter  table %s alter column type set default %L', _tbl_name, _type);
-  EXECUTE FORMAT('alter  table %s inherit resource', _tbl_name);
-  EXECUTE FORMAT('create index %s_reference_idx on %s using gin (ref(type,id))', _tbl_name, _tbl_name);
-  EXECUTE FORMAT('create index %s_id_type_idx on %s (id,type)', _tbl_name, _tbl_name);
+  EXECUTE FORMAT('create table %s partition of resource for values in (%L)', _tbl_name, _tbl_name);
+  EXECUTE FORMAT('alter table %s alter column type set default %L', _tbl_name, _tbl_name);
+
+  EXECUTE FORMAT('create index %s_reference_idx on %s using gin (ref(%L,id))', _tbl_name, _tbl_name, _tbl_name);
+  EXECUTE FORMAT('create unique index %s_udx on %s (id,last_version)', _tbl_name, _tbl_name);
+  EXECUTE FORMAT('alter table %s add primary key (key)', _tbl_name);
   
 END;
 $$ LANGUAGE plpgsql;
--- pg-store/src/main/resources/pg-store/sql/10-resource.sql
+++ pg-store/src/main/resources/pg-store/sql/10-resource.sql
@@ -4,16 +4,13 @@
 CREATE SEQUENCE resource_key_seq INCREMENT 1 MINVALUE 1;
 
 create table resource (
-  key           bigint not null default nextval('resource_key_seq') PRIMARY KEY,
+  key           bigint not null default nextval('resource_key_seq'),
   type          text not null,
   id            text not null,
   last_version  smallint not null default 1,
   last_updated  timestamp not null default localtimestamp,
   content       jsonb not null,
   sys_status    char(1) not null default 'A'
-);
-
-alter table resource add constraint resource_id_type_last_version_key unique (id,type,last_version);
-create index resource_reference_idx on resource USING gin (ref(type,id)));
+) PARTITION BY LIST (type);
 --rollback drop table resource cascade;
 
