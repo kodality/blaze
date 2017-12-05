6dfebb3247fd86c56a1f165da109a9cd28c2980b
mrcerf <mrcerf@gmail.com>
2017-12-05 16:02:36 +0200

    medications

--- etc/conf/update-definitions.sh
+++ etc/conf/update-definitions.sh
@@ -1,6 +1,25 @@
-rm definitions/*
-for res in patient practitioner encounter observation organization valueset communication medication practitionerrole; do
-  wget "http://hl7.org/fhir/$res.profile.xml" -O definitions/$res.profile.xml -q  &
+
+resources="
+patient
+practitioner
+encounter
+observation
+organization
+valueset
+communication
+practitionerrole
+medication
+medicationadministration
+medicationdispense
+medicationrequest
+medicationstatement
+immunization
+immunizationrecommendation
+"
+
+rm definitions/* || true
+for res in $resources; do
+  [[ -z "$res" ]] || wget "http://hl7.org/fhir/$res.profile.xml" -O definitions/$res.profile.xml -q  &
 done
 for job in `jobs -p`; do
   wait $job || echo "some failed"
