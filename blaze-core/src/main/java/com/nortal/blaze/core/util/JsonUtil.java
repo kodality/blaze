5b46e9abf56c030fce698cffb240e26e8fb92506
mrcerf <mrcerf@gmail.com>
2018-01-26 12:23:32 +0200

    number format exception fix

--- blaze-core/src/main/java/com/nortal/blaze/core/util/JsonUtil.java
+++ blaze-core/src/main/java/com/nortal/blaze/core/util/JsonUtil.java
@@ -2,8 +2,13 @@ package com.nortal.blaze.core.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
 import java.util.Map;
 
 public final class JsonUtil {
@@ -11,6 +16,7 @@ public final class JsonUtil {
 
   static {
     GsonBuilder builder = new GsonBuilder();
+    builder.registerTypeAdapter(Double.class, new WriteDoubleAsInt());
     gson = builder.create();
   }
 
@@ -32,6 +38,14 @@ public final class JsonUtil {
       return fromJson(new String(json, "UTF8"));
     } catch (UnsupportedEncodingException e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  private static class WriteDoubleAsInt implements JsonSerializer<Double> {
+
+    @Override
+    public JsonElement serialize(Double src, Type typeOfSrc, JsonSerializationContext context) {
+      return src == src.longValue() ? new JsonPrimitive(src.longValue()) : new JsonPrimitive(src);
     }
   }
 }
